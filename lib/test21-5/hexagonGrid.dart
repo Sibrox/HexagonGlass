e240eb6c065d4ccb327ce592d4561dff2b838152
Davide Gallotti <d.gallotti@outlook.it>
2022-05-27 19:04:06 +0200

    Added layout for hexagon Grid

--- lib/test21-5/hexagonGrid.dart
+++ lib/test21-5/hexagonGrid.dart
@@ -15,34 +15,48 @@ class _GridState extends State<Grid> {
 
   late HexagonGame gameLogic;
 
+  late double rowWidth;
+  late double hexagonWidth;
+
   @override
   void initState() {
     super.initState();
     gameLogic = widget.gameGrid;
+
+    var deviceWidth = WidgetsBinding.instance?.window.physicalSize.width;
+    var marginPer = 0.1;
+    var marginSpace = 0.0;
+    marginSpace = deviceWidth!*marginPer;
+    rowWidth = deviceWidth!-marginSpace;
+    hexagonWidth = rowWidth/gameLogic.nRows();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: List.generate(widget.gameGrid.nRows(), (i) =>
-          Row(
-            children:List.generate(widget.gameGrid.nColumns(), 
+        Positioned(
+          left: i % 2 == 0? hexagonWidth/2 : 0,
+          top: i * hexagonWidth,
+          child: Row(
+            children: List.generate(widget.gameGrid.nColumns(),
                     (j) => HexagonBottom (
-                        width: 100,
-                        num: widget.gameGrid.gameGrid[i][j].value,
-                        changeColor: () =>
-                        {
-                          setState(() {
-                            gameLogic.changeColor(i, j);
-                          })
-                        },
-                        color:
-                        gameLogic.gameGrid[i][j].color == ButtonColor.color_1 ?
-                            Colors.blue : gameLogic.gameGrid[i][j].color == ButtonColor.color_2?
-                            Colors.red : Colors.grey
-                    )
+                    width: hexagonWidth,
+                    num: widget.gameGrid.gameGrid[i][j].value,
+                    changeColor: () =>
+                    {
+                      setState(() {
+                        gameLogic.changeColor(i, j);
+                      })
+                    },
+                    color:
+                    gameLogic.gameGrid[i][j].color == ButtonColor.color_1 ?
+                    Colors.blue : gameLogic.gameGrid[i][j].color == ButtonColor.color_2?
+                    Colors.red : Colors.grey
+                )
             )
           )
+        )
       ),
     );
   }
